74ac87a5a2dd418ec528a9b80381d8264a015174
Jeetsingh Bhadauria <144542818+Jeet0814@users.noreply.github.com>
2025-04-26 12:05:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.tab').click(function () {
+        // Remove active class from all tabs
+        $('.tab').removeClass('active');
+
+        // Add active class to the clicked tab
+        $(this).addClass('active');
+
+        // Hide all tab contents
+        $('.tab-contents').removeClass('active');
+
+        // Show the selected tab content
+        $('#' + $(this).data('tab')).addClass('active');
+    });
+});
+
+
+$(document).ready(function () {
+    $(".faq-question").click(function () {
+        $(".faq-answer").not($(this).next()).slideUp();
+        $(".faq-question .icon").text("+")
+        $(this).next(".faq-answer").slideToggle();
+
+        //Change the icon (+ to -)
+        $(this).children(".icon").text(
+            $(this).next(".faq-answer").is(":visible") ? "-" : "+"
+        );
+    })
+}
+);
